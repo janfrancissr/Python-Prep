b25940485a2355586a407b99a48696ff5f99a21d
Jan Francis Arroyo Cuaquira <janfrancisac@gmail.com>
2023-05-20 13:48:31 -0500

    Upload

---
+++ 03 - Flujos de Control/Practicando_LaClase.py
@@ -0,0 +1,15 @@
+for n in range(1,10):
+    print(n)
+
+a = 1
+while (a < 10):
+    print(a)
+    a = a + 1
+
+b = -100
+if(b>0):
+    print(b, 'es numero positivo')
+elif(b<0):
+    print(b, 'es numero negativo')
+else:
+    print(b, 'es igual a cero')
